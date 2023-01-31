092d9cfa60d74148a63134f6255f3c7b37ea97c4
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-01-31 19:49:18 +0000

    final phase

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,6 +9,12 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+for rank in ranks 
+    for suit in suits
+        puts "#{rank} of #{suit}"
+    end
+end 
+
 # Sample output:
 # 2 of Clubs
 # 2 of Diamonds
@@ -20,3 +26,4 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
